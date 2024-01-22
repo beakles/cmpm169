685afc4c36c44b458a17e97ed87a9661e3c410d4
beakles <beakles@ucsc.edu>
2024-01-22 02:06:34 -0800

    Updated page format

--- experiment1/js/sketch.js
+++ experiment1/js/sketch.js
@@ -5,6 +5,18 @@
 // Global variables
 let waveIncrement = 0;
 let shapesArray = [];
+let canvasContainer; // Container for the canvas
+let possibleShapeProperties = { // Randomized properties for generated shapes
+  sides: 0,
+  size: 0,
+  rotation: 0,
+  rotationChange: 0,
+  warpSpeed: 0,
+  warpScale: 0,
+  warpDirection: 0,
+  shapeOriginX: 0,
+  shapeOriginY: 0,
+}
 
 // Shape point class
 class ShapePoint {
@@ -113,21 +125,6 @@ class Shape {
   }
 }
 
-// Variable for the canvas
-let canvasContainer;
-
-// Randomized properties for generated shapes
-let possibleShapeProperties = {
-  sides: 0,
-  size: 0,
-  rotation: 0,
-  rotationChange: 0,
-  warpSpeed: 0,
-  warpScale: 0,
-  warpDirection: 0,
-  shapeOriginX: 0,
-  shapeOriginY: 0,
-}
 
 // Randomize the properties for the shape
 function randomizeShapeProperties() {
@@ -143,25 +140,23 @@ function randomizeShapeProperties() {
 }
 
 function setup() {
-
   // Create a canvas for the vector germs
   canvasContainer = $("#canvas-container");
-  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-  canvas.parent("canvas-container");
-
+  let canvasNew = createCanvas(canvasContainer.width(), 800);
+  canvasNew.parent("canvas-container");
+  // resize canvas is the page is resized
   $(window).resize(function() {
-    // console.log("Resizing...");
-    resizeCanvas(canvasContainer.width(), canvasContainer.height());
+      console.log("Resizing...");
+      resizeCanvas(canvasContainer.width(), canvasContainer.height());
   });
   
   // Randomized shape properties
   randomizeShapeProperties();
   
-  // Spawn a few example shapes
+  // Spawn a few sample shapes
   for (let increment = 0; increment < 3; increment += 1) {
     let shapeNew = new Shape("geometry", possibleShapeProperties.sides, possibleShapeProperties.size, possibleShapeProperties.rotation, possibleShapeProperties.rotationChange, possibleShapeProperties.warpSpeed, possibleShapeProperties.warpScale, possibleShapeProperties.warpDirection, possibleShapeProperties.shapeOriginX, possibleShapeProperties.shapeOriginY);
     shapesArray.push(shapeNew);
-
     randomizeShapeProperties();
   }
 }
@@ -169,13 +164,6 @@ function setup() {
 // Update the shapes on screen every frame
 function draw() {
   background(20, 20, 20);
-  
-  /*
-  rectMode(CENTER);
-  textSize(32);
-  text('Vector germs', width / 2 - 64, height / 2);
-  describe("Vector germs");
-  */
   
   for (let increment = 0; increment < shapesArray.length; increment += 1) {
     shapesArray[increment].updateShape();
@@ -187,79 +175,8 @@ function draw() {
 
 // Spawn a new shape on mouse click
 function mouseClicked() {
-
   randomizeShapeProperties();
   
   let shapeNew = new Shape("geometry", possibleShapeProperties.sides, possibleShapeProperties.size, possibleShapeProperties.rotation, possibleShapeProperties.rotationChange, possibleShapeProperties.warpSpeed, possibleShapeProperties.warpScale, possibleShapeProperties.warpDirection, possibleShapeProperties.shapeOriginX, possibleShapeProperties.shapeOriginY);
   shapesArray.push(shapeNew);
 }
-
-/*
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
-
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
-
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
-
-// setup() function is called once when the program starts
-function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
-
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
-}
-
-// draw() function is called repeatedly, it's the main animation loop
-function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
-
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
-}
-
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
-}
-*/
