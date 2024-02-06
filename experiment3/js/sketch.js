fd056974e81acad87c9c5bdfe2ab9024be9967a1
beakles <beakles@ucsc.edu>
2024-02-05 23:33:14 -0800

    Added code to website

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -1,67 +1,156 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+let videoFeed;
+let microphone;
+let microphoneLevelMap;
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
-
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
+let settings = {
+  videoUpdateFrequency: 1 / 24,
+  videoUpdateFrequencyMicActive: 2,
+  microphoneLevelAmplifier: 1,
+  microphoneLevelThreshold: 20,
+  distortionSizeCap: 200,
 }
 
-// setup() function is called once when the program starts
+let textEnabledTime = 1;
+let textEnabledDebounce = 0;
+let textEnabledBool = false;
+let glitchedBloomDebounce = 0;
+
+let videoUpdateDebounce = 0;
+let currentVideoUpdateFrequency = settings.videoUpdateFrequency;
+
+let sound1;
+let sound2;
+let sound3;
+let sound4;
+
+function preload() {
+  sound1 = loadSound("../../../assets/cameraRestart.wav");
+  sound2 = loadSound("../../../assets/cameraShutdown.wav");
+  sound3 = loadSound("../../../assets/click.wav");
+  sound4 = loadSound("../../../assets/staticSpawn.wav");
+}
+
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
+  createCanvas(600, 600);
+  
+  background(0, 0, 0);
+  
+  videoFeed = createCapture(VIDEO);
+  videoFeed.size(width, height);
+  videoFeed.hide();
+  
+  microphone = new p5.AudioIn();
+  microphone.start();
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+  sound1.play();
+  sound2.play();
+  sound3.play();
+  sound4.play();
 }
 
-// draw() function is called repeatedly, it's the main animation loop
+function scatterScreen(times, size) {
+  if (size <= 1) {
+    size = 1;
+  }
+  
+  for (let increment = 0; increment < times; increment += 1) {
+    let subImageDistortionX = random(width);
+    let subImageDistortionY = random(height);
+
+    let subImageDistortionXNew = round(subImageDistortionX + random(-size, size));
+    let subImageDistortionYNew = round(subImageDistortionY + random(-size, size));
+
+    set(subImageDistortionXNew, subImageDistortionYNew, get(subImageDistortionX, subImageDistortionY, size, size));
+  }
+}
+
+function drawCrosshair() {
+  fill(255, 255, 255, 255);
+  stroke(0, 0, 0, 0);
+  rect(width - 110, height - 20, 100, 10);
+  rect(width - 20, height - 110, 10, 100);
+
+  rect(0 + 10, height - 20, 100, 10);
+  rect(0 + 10, height - 110, 10, 100);
+
+  rect(width - 110, 0 + 10, 100, 10);
+  rect(width - 20, 0 + 10, 10, 100);
+
+  rect(0 + 10, 0 + 10, 100, 10);
+  rect(0 + 10, 0 + 10, 10, 100);
+
+  rect(width / 2 - 18, height / 2, 40, 5);
+  rect(width / 2, height / 2 - 18, 5, 40);
+}
+
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+  if (videoUpdateDebounce >= currentVideoUpdateFrequency) {
+    if (currentVideoUpdateFrequency >= settings.videoUpdateFrequencyMicActive) {
+      fill(255, 0, 0, 255);
+      stroke(0, 0, 0, 0);
+      rect(0, 0, width, height)
+      currentVideoUpdateFrequency = settings.videoUpdateFrequency;
+    } else {
+      videoUpdateDebounce = 0;
+      currentVideoUpdateFrequency = settings.videoUpdateFrequency;
+      image(videoFeed, 0, 0, width, height);
+        
+      drawCrosshair();
+      
+      filter(GRAY);
+      
+      if (textEnabledBool) {
+        fill(255, 0, 0, 170);
+        stroke(0, 0, 0, 0);
+        textFont("Courier New");
+        textSize(24);
+        textStyle(BOLD);
+        textAlign(CENTER);
+        text("Recording", 130, 35 + (24 / 2));
+        fill(255, 0, 0, 170);
+        ellipse(40, 40, 20, 20);
+      } else {
+        fill(255, 0, 0, 170);
+        ellipse(40, 40, 20, 20);
+      }
+      
+      glitchedBloomDebounce = random(0, 100);
+      if (glitchedBloomDebounce <= 10) {
+        let randomEffect = random(0, 100);
+        if (randomEffect <= 50) {
+          filter(DILATE);
+        } else {
+          filter(THRESHOLD);
+        }
+      }
+      scatterScreen(10, 10);
+    }
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
-}
-
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+  } else {
+    videoUpdateDebounce += 1 / 60;
+  }
+  
+  if (textEnabledDebounce >= textEnabledTime) {
+    textEnabledDebounce = 0;
+    textEnabledBool = !textEnabledBool;
+  } else {
+    textEnabledDebounce += 1 / 60;
+  }
+  
+  let microphoneLevel = microphone.getLevel() * settings.microphoneLevelAmplifier;
+  microphoneLevelMap = map(microphoneLevel, 0, 1, 0, settings.distortionSizeCap);
+  microphoneLevelMap = floor(microphoneLevelMap);
+  
+  print(microphoneLevelMap);
+  
+  if (microphoneLevelMap >= settings.microphoneLevelThreshold) {
+    videoUpdateDebounce = 0;
+    currentVideoUpdateFrequency = settings.videoUpdateFrequencyMicActive;
+  }
+  
+  if (currentVideoUpdateFrequency >= settings.videoUpdateFrequencyMicActive && microphoneLevelMap < settings.microphoneLevelThreshold) {
+    scatterScreen(7, (width / 3) * (videoUpdateDebounce / currentVideoUpdateFrequency));
+  }
+  
+  scatterScreen(microphoneLevelMap, microphoneLevelMap);
 }
